3865f421a830db949d0e22e79b17f425bd2cf533
AfrojKhanPathan <pafrojkhan@gmail.com>
2021-08-10 13:33:23 +0530

    fixed error

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from app import create_app,db
+from app.auth.model import user
+from sqlalchemy import exc
 
     app = create_app('prod')
+
     with app.app_context():
         db.create_all()
-
-    app.run()
+        try:
+            if not User.query.filter_by(user_name="harry").first():
+                User.create_user(user='harry',email='harry@pooters.com',password='secret')
+        except exc.IntegrityError:
+            app.run()
